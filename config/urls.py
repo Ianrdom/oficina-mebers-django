5182d599d1342b4b6be5a4d42c69c465ddc182b3
Ian3INFO2 <ianrobertoestudos10@gmail.com>
2023-10-10 15:06:02 -0300

    pouca coisa mexida, só arrumando url do token

--- config/urls.py
+++ config/urls.py
@@ -38,8 +38,12 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api/", include(usuario_router.urls)),
     path("api/media/", include(uploader_router.urls)),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
